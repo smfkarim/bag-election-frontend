f84b6d7461bb81cc1c65e426dd8bed9ab65fe41a
albiummid <albi.ummid@gmail.com>
2025-11-17 15:52:20 +0600

    fix: manual ballot paper optimized (images,layout)

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,18 @@
 import type { NextConfig } from "next";
-
+const bucketUrl = process.env.NEXT_PUBLIC_BUCKET_URL || "";
+const { hostname } = new URL(bucketUrl);
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+    /* config options here */
+    reactCompiler: true,
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: hostname, // dynamic based on env
+                pathname: "/**",
+            },
+        ],
+    },
 };
 
 export default nextConfig;
